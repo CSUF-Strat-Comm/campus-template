fe1c11a1b66c430accc9be544c7fbffa50ce5359
Robert Rodriguez <robert@robertrodriguez.io>
2021-08-02 14:53:14 -0700

    Add new window icon to links w/ target="_blank"

--- _assets/js/components/general.js
+++ _assets/js/components/general.js
@@ -3,4 +3,7 @@ $(function () {
   $('.js-accordion').accordion({ 
     multiselectable: false
   });
+
+  $("a[target='_blank']").append('&nbsp;<span class="material-icons-outlined text-size-sm" title="Opens in new window">open_in_new</span>');
+
 });
